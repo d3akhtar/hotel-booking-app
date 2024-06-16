09ccf42dc2bfad3540d2d883d7988e0d02e174be
d3akhtar <danyal.akhtar@torontomu.ca>
2024-06-15 20:56:14 -0400

    layout setup, more examples of tailwindcss use

---
+++ frontend/src/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Footer, Header, Hero } from '../components'
+
+interface Props {
+    children: React.ReactNode;
+}
+
+function Layout({children}: Props) {
+  return (
+    <div className='flex flex-col min-h-screen'>
+        <Header/>
+        <Hero/>
+        <div className='container mx-auto py-10 flex-1'> 
+            {children}
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+// flex-1 makes it so the div takes up the available space
+
+export default Layout
